9775e4d27d4dda78e6b9c00180e2118ae431b968
Alexey Nesterov <vertigra.developer@gmail.com>
2022-08-19 08:36:36 +1000

    Refact elbow func

--- InterpolationMethod.py
+++ InterpolationMethod.py
@@ -1,12 +1,11 @@
 from ReadHomPos import ReadHomPos
 from calcPostiotionsArms import CalcPositionsArms
 
-class calcPosServo():
+class CalcPosServo():
     def __init__(self):
         self._ReadPosArray = ReadHomPos()
         self._HomePositionArray = self._ReadPosArray.ReadServRange()
         self._PositionArms = CalcPositionsArms(self._HomePositionArray)
-
 
     def CalcHead(self, GoalPosHeadPer, GoalPosNeckPer):
 
@@ -20,20 +19,19 @@ class calcPosServo():
      
         return CalcGoalPosHead, CalcGoalPosNeck
 
-   def CalcArms(self, ArmShoulderAnglRightPer, ArmForearmAnglRightPer, ArmElbowTopAnglRightPer, ArmElbowBottAnglRightPer, ArmElbowAnglRightPer,
+    def CalcArms(self, ArmShoulderAnglRightPer, ArmForearmAnglRightPer, ArmElbowTopAnglRightPer, ArmElbowBottAnglRightPer, ArmElbowAnglRightPer,
                 ArmShoulderAnglLeftPer, ArmForearmAnglLeftPer, ArmElbowTopAnglLeftPer, ArmElbowBottAnglLeftPer, ArmElbowAnglLeftPer, SwitchBothElbow):
 
         if SwitchBothElbow == False:
             ArmRight = self._PositionArms.CalcArmsRight(ArmShoulderAnglRightPer, ArmForearmAnglRightPer, ArmElbowTopAnglRightPer, ArmElbowBottAnglRightPer)
             ArmLeft = self._PositionArms.CalcArmsLeft(ArmShoulderAnglLeftPer, ArmForearmAnglLeftPer, ArmElbowTopAnglLeftPer, ArmElbowBottAnglLeftPer)
         else:
-            ArmRight = self._PositionArms.CalcArmsRight(ArmShoulderAnglRightPer, ArmForearmAnglRightPer, ArmElbowAnglRightPer)
-            ArmLeft = self._PositionArms.CalcArmsLeft(ArmShoulderAnglLeftPer, ArmForearmAnglLeftPer,ArmElbowAnglLeftPer)
+            ArmRight = self._PositionArms.CalcArmsRightElbow(ArmShoulderAnglRightPer, ArmForearmAnglRightPer, ArmElbowAnglRightPer)
+            ArmLeft = self._PositionArms.CalcArmsLeftElbow(ArmShoulderAnglLeftPer, ArmForearmAnglLeftPer,ArmElbowAnglLeftPer)
 
         return ('RightArm', ArmRight[0], ArmRight[1], ArmRight[2], ArmRight[3], 'LeftArm', ArmLeft[0], ArmLeft[1], ArmLeft[2], ArmLeft[3]) 
 
     def CalcPress(self, PressTopAnglPer, PressBottAnglPer):
-
         PressTopAnglMin  =  self._HomePositionArray[3][0][0]
         PressTopAnglMax  =  self._HomePositionArray[3][1][0]
         PressBottAnglMin =  self._HomePositionArray[3][2][0]
@@ -53,7 +51,6 @@ class calcPosServo():
         return CalcGoalPosBody
 
     def CalcLeg(self, HightBothLegPer, HightLeftLegPer, HightRightLegPer, SwitchBothLeg):
-
         LegHipAngRightMin = self._HomePositionArray[5][4][0]
         LegHipAngRightMax = self._HomePositionArray[5][5][0]
         LegFootAngRightMin = self._HomePositionArray[5][2][0]
@@ -69,24 +66,21 @@ class calcPosServo():
         LegKneeAngLeftMax = self._HomePositionArray[6][1][0]
 
         if SwitchBothLeg == False:
+            CalcGoalPosHipRight = ((LegHipAngRightMax - LegHipAngRightMin) * HightRightLegPer) + LegHipAngRightMin
+            CalcGoalPosFootRight = ((LegFootAngRightMax - LegFootAngRightMin) * HightRightLegPer) + LegFootAngRightMin
+            CalcGoalPosKneeRight = ((LegKneeAngRightMax - LegKneeAngRightMin) * HightRightLegPer) + LegKneeAngRightMin
 
-             CalcGoalPosHipRight = ((LegHipAngRightMax - LegHipAngRightMin) * HightRightLegPer) + LegHipAngRightMin
-             CalcGoalPosFootRight = ((LegFootAngRightMax - LegFootAngRightMin) * HightRightLegPer) + LegFootAngRightMin
-             CalcGoalPosKneeRight = ((LegKneeAngRightMax - LegKneeAngRightMin) * HightRightLegPer) + LegKneeAngRightMin
-
-             CalcGoalPosHipLeft = ((LegHipAngLeftMax - LegHipAngLeftMin) * HightLeftLegPer) + LegHipAngLeftMin
-             CalcGoalPosFootLeft = ((LegFootAngLeftMax - LegFootAngLeftMin) * HightLeftLegPer) + LegFootAngLeftMin
-             CalcGoalPosKneeLeft = ((LegKneeAngLeftMax - LegKneeAngLeftMin) * HightLeftLegPer) + LegKneeAngLeftMin
-          
+            CalcGoalPosHipLeft = ((LegHipAngLeftMax - LegHipAngLeftMin) * HightLeftLegPer) + LegHipAngLeftMin
+            CalcGoalPosFootLeft = ((LegFootAngLeftMax - LegFootAngLeftMin) * HightLeftLegPer) + LegFootAngLeftMin
+            CalcGoalPosKneeLeft = ((LegKneeAngLeftMax - LegKneeAngLeftMin) * HightLeftLegPer) + LegKneeAngLeftMin
         else:
-         
-             CalcGoalPosHipRight = ((LegHipAngRightMax - LegHipAngRightMin) * HightBothLegPer) + LegHipAngRightMin
-             CalcGoalPosFootRight = ((LegFootAngRightMax - LegFootAngRightMin) * HightBothLegPer) + LegFootAngRightMin
-             CalcGoalPosKneeRight = ((LegKneeAngRightMax - LegKneeAngRightMin) * HightBothLegPer) + LegKneeAngRightMin
+            CalcGoalPosHipRight = ((LegHipAngRightMax - LegHipAngRightMin) * HightBothLegPer) + LegHipAngRightMin
+            CalcGoalPosFootRight = ((LegFootAngRightMax - LegFootAngRightMin) * HightBothLegPer) + LegFootAngRightMin
+            CalcGoalPosKneeRight = ((LegKneeAngRightMax - LegKneeAngRightMin) * HightBothLegPer) + LegKneeAngRightMin
 
-             CalcGoalPosHipLeft = ((LegHipAngLeftMax - LegHipAngLeftMin) * HightBothLegPer) + LegHipAngLeftMin
-             CalcGoalPosFootLeft = ((LegFootAngLeftMax - LegFootAngLeftMin) * HightBothLegPer) + LegFootAngLeftMin
-             CalcGoalPosKneeLeft = ((LegKneeAngLeftMax - LegKneeAngLeftMin) * HightBothLegPer) + LegKneeAngLeftMin
+            CalcGoalPosHipLeft = ((LegHipAngLeftMax - LegHipAngLeftMin) * HightBothLegPer) + LegHipAngLeftMin
+            CalcGoalPosFootLeft = ((LegFootAngLeftMax - LegFootAngLeftMin) * HightBothLegPer) + LegFootAngLeftMin
+            CalcGoalPosKneeLeft = ((LegKneeAngLeftMax - LegKneeAngLeftMin) * HightBothLegPer) + LegKneeAngLeftMin
 
         return 'RightLeg', CalcGoalPosHipRight, CalcGoalPosFootRight, CalcGoalPosKneeRight, 'LeftLeg', CalcGoalPosHipLeft, CalcGoalPosFootLeft, CalcGoalPosKneeLeft
 
--- calcPostiotionsArms.py
+++ calcPostiotionsArms.py
@@ -1,34 +1,24 @@
 
 class CalcPositionsArms():
-	def __init__(self, ArmRangeArray):
-		
+    def __init__(self, ArmRangeArray):		
 		self._ArmShoulderRightMin = ArmRangeArray[1][0][0]
         self._ArmShoulderRightMax = ArmRangeArray[1][1][0]
-        
-		self._ArmForearmRightMin  = ArmRangeArray[1][2][0]
+        self._ArmForearmRightMin  = ArmRangeArray[1][2][0]
         self._ArmForearmRightMax  = ArmRangeArray[1][3][0]
-        
-		self._ArmElbowTopRightMin = ArmRangeArray[1][4][0]
+        self._ArmElbowTopRightMin = ArmRangeArray[1][4][0]
         self._ArmElbowTopRightMax = ArmRangeArray[1][5][0]
-        
-		self._ArmElbowBottRightMin = ArmRangeArray[1][6][0]
+        self._ArmElbowBottRightMin = ArmRangeArray[1][6][0]
         self._ArmElbowBottRightMax = ArmRangeArray[1][7][0]
-
-        
-		self._ArmShoulderLeftMax = ArmRangeArray[2][0][0] #electronic range exclusion
+        self._ArmShoulderLeftMax = ArmRangeArray[2][0][0] #electronic range exclusion
         self._ArmShoulderLeftMin = ArmRangeArray[2][1][0] #electronic range exclusion
-        
-		self._ArmForearmLeftMin  = ArmRangeArray[2][2][0]
+        self._ArmForearmLeftMin  = ArmRangeArray[2][2][0]
         self._ArmForearmLeftMax  = ArmRangeArray[2][3][0]
-
         self._ArmElbowTopLeftMin = ArmRangeArray[2][4][0]
         self._ArmElbowTopLeftMax = ArmRangeArray[2][5][0]
-
         self._ArmElbowBottLeftMin = ArmRangeArray[2][6][0]
         self._ArmElbowBottLeftMax = ArmRangeArray[2][7][0]
 
-
-	def CalcArmsRight(ArmShoulderAnglePer, ArmForearmAnglePer, ArmElbowTopAnglePer, ArmElbowBottomAnglePer):
+    def CalcArmsRight(ArmShoulderAnglePer, ArmForearmAnglePer, ArmElbowTopAnglePer, ArmElbowBottomAnglePer):
         CalcGolPosShoulderRight = self._CalcArmsAngle(self._ArmShoulderRightMax, self._ArmShoulderRightMin, ArmShoulderAnglePer)
         CalcGolPosForearmRight = self._CalcArmsAngle(self._ArmForearmRightMax, self._ArmForearmRightMin, ArmForearmAnglePer)
         CalcGolPosElbowTopRight =  self._CalcArmsAngle(self._ArmElbowTopRightMax, self._ArmForearmRightMin, ArmElbowTopAnglePer)
@@ -36,8 +26,8 @@ class CalcPositionsArms():
 
         return (CalcGolPosShoulderRight, CalcGolPosForearmRight, CalcGolPosElbowTopRight, CalcGolPosElbowBottRight) 
 
-	def CalcArmsLeft(ArmShoulderAnglePer, ArmForearmAnglePer, ArmElbowTopAnglePer, ArmElbowBottomAnglePer):
-		CalcGolPosShoulderLeft = ((self._ArmShoulderLeftMax - self._ArmShoulderLeftMin) * ArmShoulderAnglePer) + self._ArmShoulderLeftMin
+    def CalcArmsLeft(ArmShoulderAnglePer, ArmForearmAnglePer, ArmElbowTopAnglePer, ArmElbowBottomAnglePer):
+        CalcGolPosShoulderLeft = ((self._ArmShoulderLeftMax - self._ArmShoulderLeftMin) * ArmShoulderAnglePer) + self._ArmShoulderLeftMin
         CalcGolPosForearmLeft = ((self._ArmForearmLeftMax - self._ArmForearmLeftMin) * ArmForearmAnglePer) + self._ArmForearmLeftMin
         CalcGolPosElbowTopLeft = ((self._ArmElbowTopLeftMax - self._ArmElbowTopLeftMin) * ArmElbowTopAnglePer) + self._ArmElbowTopLeftMin
         CalcGolPosElbowBottLeft = ((self._ArmElbowBottLeftMax - self._ArmElbowBottLeftMin) * ArmElbowBottomAnglePer) + self._ArmElbowBottLeftMin
@@ -45,7 +35,7 @@ class CalcPositionsArms():
         return (CalcGolPosShoulderLeft, CalcGolPosForearmLeft, CalcGolPosElbowTopLeft, CalcGolPosElbowBottLeft) 
 
     #elbow
-    def CalcArmsRight(ArmShoulderAnglePer, ArmForearmAnglePer, ArmElbowAnglePer):
+    def CalcArmsRightElbow(ArmShoulderAnglePer, ArmForearmAnglePer, ArmElbowAnglePer):
         CalcGolPosShoulderRight = self._CalcArmsAngle(self._ArmShoulderRightMax, self._ArmShoulderRightMin, ArmShoulderAnglePer)
         CalcGolPosForearmRight = self._CalcArmsAngle(self._ArmForearmRightMax, self._ArmForearmRightMin, ArmForearmAnglePer)
         CalcGolPosElbowTopRight =  self._CalcArmsAngle(self._ArmElbowTopRightMax, self._ArmElbowTopRightMin, ArmElbowAnglePer)
@@ -54,14 +44,14 @@ class CalcPositionsArms():
         return (CalcGolPosShoulderRight, CalcGolPosForearmRight, CalcGolPosElbowTopRight, CalcGolPosElbowBottRight) 
 
     #elbow
-	def CalcArmsLeft(ArmShoulderAnglePer, ArmForearmAnglePer, ArmElbowAnglePer):
-		CalcGolPosShoulder = self._CalcArmsAngle(self._ArmShoulderLeftMax, self._ArmShoulderLeftMin, ArmShoulderAnglePer)
+    def CalcArmsLeftElbow(ArmShoulderAnglePer, ArmForearmAnglePer, ArmElbowAnglePer):
+        CalcGolPosShoulder = self._CalcArmsAngle(self._ArmShoulderLeftMax, self._ArmShoulderLeftMin, ArmShoulderAnglePer)
         CalcGolPosForearm = self._CalcArmsAngle(self._ArmForearmLeftMax, self._ArmForearmLeftMin, ArmForearmAnglePer)
         CalcGolPosElbowTop =  self._CalcArmsAngle(self._ArmElbowTopLeftMax, self._ArmElbowTopLeftMin, ArmElbowAnglePer)
         CalcGolPosElbowBottRight = self._CalcArmsAngle(self._ArmElbowBottLeftMax, self._ArmElbowBottLeftMin, ArmElbowAnglePer) 
         
         return (CalcGolPosShoulderLeft, CalcGolPosForearmLeft, CalcGolPosElbowTopLeft, CalcGolPosElbowBottLeft) 
 
-	def _CalcArmsAngle(angleMax, angleMin, anglePer):
-		position = ((angleMax - angleMin) * anglePer) + angleMin
-		return position
+    def _CalcArmsAngle(angleMax, angleMin, anglePer):
+        position = ((angleMax - angleMin) * anglePer) + angleMin
+        return position
