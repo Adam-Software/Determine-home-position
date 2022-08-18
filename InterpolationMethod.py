2592df0daa0c4aca27eb10a70d3e507923bac8c7
Alexey Nesterov <vertigra.developer@gmail.com>
2022-08-18 15:50:29 +1000

    Refact

--- InterpolationMethod.py
+++ InterpolationMethod.py
@@ -1,88 +1,43 @@
 from ReadHomPos import ReadHomPos
+from calcPostiotionsArms import CalcPositionsArms
 
 class calcPosServo():
     def __init__(self):
-        super().__init__()
+        self._ReadPosArray = ReadHomPos()
+        self._HomePositionArray = self._ReadPosArray.ReadServRange()
+        self._PositionArms = CalcPositionsArms(self._HomePositionArray)
 
 
     def CalcHead(self, GoalPosHeadPer, GoalPosNeckPer):
 
-        ReadPosArray = ReadHomPos()
-             
-        HeadRangeArray = ReadPosArray.ReadServRange()
-    
-        HeadRangeMin = HeadRangeArray[0][0][0]
-        HeadRangeMax = HeadRangeArray[0][1][0]
-        NeckRangeMin = HeadRangeArray[0][2][0]
-        NeckRangeMax = HeadRangeArray[0][3][0]
+        HeadRangeMin = self._HomePositionArray[0][0][0]
+        HeadRangeMax = self._HomePositionArray[0][1][0]
+        NeckRangeMin = self._HomePositionArray[0][2][0]
+        NeckRangeMax = self._HomePositionArray[0][3][0]
     
         CalcGoalPosHead = ((HeadRangeMax - HeadRangeMin) * GoalPosHeadPer) + HeadRangeMin
         CalcGoalPosNeck = ((NeckRangeMax - NeckRangeMin) * GoalPosNeckPer) + NeckRangeMin
      
         return CalcGoalPosHead, CalcGoalPosNeck
 
-    def CalcArms(self, ArmShoulderAnglRightPer, ArmForearmAnglRightPer, ArmElbowTopAnglRightPer, ArmElbowBottAnglRightPer, ArmElbowAnglRightPer,
-                 ArmShoulderAnglLeftPer, ArmForearmAnglLeftPer, ArmElbowTopAnglLeftPer, ArmElbowBottAnglLeftPer, ArmElbowAnglLeftPer, SwitchBothlbow):
+   def CalcArms(self, ArmShoulderAnglRightPer, ArmForearmAnglRightPer, ArmElbowTopAnglRightPer, ArmElbowBottAnglRightPer, ArmElbowAnglRightPer,
+                ArmShoulderAnglLeftPer, ArmForearmAnglLeftPer, ArmElbowTopAnglLeftPer, ArmElbowBottAnglLeftPer, ArmElbowAnglLeftPer, SwitchBothElbow):
 
-        ReadPosArray = ReadHomPos()
-
-        ArmRangeArray  = ReadPosArray.ReadServRange()
-  
-        ArmShoulderRightMin = ArmRangeArray[1][0][0]
-        ArmShoulderRightMax = ArmRangeArray[1][1][0]
-        ArmForearmRightMin  = ArmRangeArray[1][2][0]
-        ArmForearmRightMax  = ArmRangeArray[1][3][0]
-        ArmElbowTopRightMin = ArmRangeArray[1][4][0]
-        ArmElbowTopRightMax = ArmRangeArray[1][5][0]
-        ArmElbowBottRightMin = ArmRangeArray[1][6][0]
-        ArmElbowBottRightMax = ArmRangeArray[1][7][0]
-
-        ArmShoulderLeftMax = ArmRangeArray[2][0][0] #electronic range exclusion
-        ArmShoulderLeftMin = ArmRangeArray[2][1][0] #electronic range exclusion
-        ArmForearmLeftMin  = ArmRangeArray[2][2][0]
-        ArmForearmLeftMax  = ArmRangeArray[2][3][0]
-        ArmElbowTopLeftMin = ArmRangeArray[2][4][0]
-        ArmElbowTopLeftMax = ArmRangeArray[2][5][0]
-        ArmElbowBottLeftMin = ArmRangeArray[2][6][0]
-        ArmElbowBottLeftMax = ArmRangeArray[2][7][0]
-
-        if SwitchBothlbow == False:
-
-            CalcGolPosShoulderRight = ((ArmShoulderRightMax - ArmShoulderRightMin) * ArmShoulderAnglRightPer) + ArmShoulderRightMin
-            CalcGolPosForearmRight = ((ArmForearmRightMax - ArmForearmRightMin) * ArmForearmAnglRightPer) + ArmForearmRightMin
-            CalcGolPosElbowTopRight = ((ArmElbowTopRightMax - ArmElbowTopRightMin) * ArmElbowTopAnglRightPer) + ArmElbowTopRightMin
-            CalcGolPosElbowBottRight = ((ArmElbowBottRightMax - ArmElbowBottRightMin) * ArmElbowBottAnglRightPer) + ArmElbowBottRightMin
-
-            CalcGolPosShoulderLeft = ((ArmShoulderLeftMax - ArmShoulderLeftMin) * ArmShoulderAnglLeftPer) + ArmShoulderLeftMin
-            CalcGolPosForearmLeft = ((ArmForearmLeftMax - ArmForearmLeftMin) * ArmForearmAnglLeftPer) + ArmForearmLeftMin
-            CalcGolPosElbowTopLeft = ((ArmElbowTopLeftMax - ArmElbowTopLeftMin) * ArmElbowTopAnglLeftPer) + ArmElbowTopLeftMin
-            CalcGolPosElbowBottLeft = ((ArmElbowBottLeftMax - ArmElbowBottLeftMin) * ArmElbowBottAnglLeftPer) + ArmElbowBottLeftMin
-
+        if SwitchBothElbow == False:
+            ArmRight = self._PositionArms.CalcArmsRight(ArmShoulderAnglRightPer, ArmForearmAnglRightPer, ArmElbowTopAnglRightPer, ArmElbowBottAnglRightPer)
+            ArmLeft = self._PositionArms.CalcArmsLeft(ArmShoulderAnglLeftPer, ArmForearmAnglLeftPer, ArmElbowTopAnglLeftPer, ArmElbowBottAnglLeftPer)
         else:
+            ArmRight = self._PositionArms.CalcArmsRight(ArmShoulderAnglRightPer, ArmForearmAnglRightPer, ArmElbowAnglRightPer)
+            ArmLeft = self._PositionArms.CalcArmsLeft(ArmShoulderAnglLeftPer, ArmForearmAnglLeftPer,ArmElbowAnglLeftPer)
 
-            CalcGolPosShoulderRight = ((ArmShoulderRightMax - ArmShoulderRightMin) * ArmShoulderAnglRightPer) + ArmShoulderRightMin
-            CalcGolPosForearmRight = ((ArmForearmRightMax - ArmForearmRightMin) * ArmForearmAnglRightPer) + ArmForearmRightMin
-            CalcGolPosElbowTopRight = ((ArmElbowTopRightMax - ArmElbowTopRightMin) * ArmElbowAnglRightPer) + ArmElbowTopRightMin
-            CalcGolPosElbowBottRight = ((ArmElbowBottRightMax - ArmElbowBottRightMin) * ArmElbowAnglRightPer) + ArmElbowBottRightMin
-
-            CalcGolPosShoulderLeft = ((ArmShoulderLeftMax - ArmShoulderLeftMin) * ArmShoulderAnglLeftPer) + ArmShoulderLeftMin
-            CalcGolPosForearmLeft = ((ArmForearmLeftMax - ArmForearmLeftMin) * ArmForearmAnglLeftPer) + ArmForearmLeftMin
-            CalcGolPosElbowTopLeft = ((ArmElbowTopLeftMax - ArmElbowTopLeftMin) * ArmElbowAnglLeftPer) + ArmElbowTopLeftMin
-            CalcGolPosElbowBottLeft = ((ArmElbowBottLeftMax - ArmElbowBottLeftMin) * ArmElbowAnglLeftPer) + ArmElbowBottLeftMin
-     
-        return ('RightArm', CalcGolPosShoulderRight, CalcGolPosForearmRight, CalcGolPosElbowTopRight, CalcGolPosElbowBottRight, 
-                'LeftArm', CalcGolPosShoulderLeft, CalcGolPosForearmLeft, CalcGolPosElbowTopLeft, CalcGolPosElbowBottLeft) 
+        return ('RightArm', ArmRight[0], ArmRight[1], ArmRight[2], ArmRight[3], 'LeftArm', ArmLeft[0], ArmLeft[1], ArmLeft[2], ArmLeft[3]) 
 
     def CalcPress(self, PressTopAnglPer, PressBottAnglPer):
-    
-        ReadPosArray = ReadHomPos()
-             
-        PressRangeArray  = ReadPosArray.ReadServRange()
 
-        PressTopAnglMin  =  PressRangeArray[3][0][0]
-        PressTopAnglMax  =  PressRangeArray[3][1][0]
-        PressBottAnglMin =  PressRangeArray[3][2][0]
-        PressBottAnglMax =  PressRangeArray[3][3][0]
+        PressTopAnglMin  =  self._HomePositionArray[3][0][0]
+        PressTopAnglMax  =  self._HomePositionArray[3][1][0]
+        PressBottAnglMin =  self._HomePositionArray[3][2][0]
+        PressBottAnglMax =  self._HomePositionArray[3][3][0]
 
         CalcGoalPosPressTop = ((PressTopAnglMax - PressTopAnglMin) * PressTopAnglPer) + PressTopAnglMin
         CalcGoalPosPressBott = ((PressBottAnglMax - PressBottAnglMin) * PressBottAnglPer) + PressBottAnglMin
@@ -90,13 +45,8 @@ class calcPosServo():
         return CalcGoalPosPressTop, CalcGoalPosPressBott
 
     def CalcBody(self, BodyRotPer):
-
-        ReadPosArray = ReadHomPos()
-             
-        BodyRotArray  = ReadPosArray.ReadServRange()
-    
-        BodyRotMin  =  BodyRotArray[4][0][0]
-        BodyRotMax  =  BodyRotArray[4][1][0]
+        BodyRotMin  =  self._HomePositionArray[4][0][0]
+        BodyRotMax  =  self._HomePositionArray[4][1][0]
 
         CalcGoalPosBody = ((BodyRotMax - BodyRotMin) * BodyRotPer) + BodyRotMin
 
@@ -104,23 +54,19 @@ class calcPosServo():
 
     def CalcLeg(self, HightBothLegPer, HightLeftLegPer, HightRightLegPer, SwitchBothLeg):
 
-        ReadPosArray = ReadHomPos()
+        LegHipAngRightMin = self._HomePositionArray[5][4][0]
+        LegHipAngRightMax = self._HomePositionArray[5][5][0]
+        LegFootAngRightMin = self._HomePositionArray[5][2][0]
+        LegFootAngRightMax = self._HomePositionArray[5][3][0]
+        LegKneeAngRightMin = self._HomePositionArray[5][0][0]
+        LegKneeAngRightMax = self._HomePositionArray[5][1][0]
 
-        LegRangeArray = ReadPosArray.ReadServRange()
-
-        LegHipAngRightMin = LegRangeArray[5][4][0]
-        LegHipAngRightMax = LegRangeArray[5][5][0]
-        LegFootAngRightMin = LegRangeArray[5][2][0]
-        LegFootAngRightMax = LegRangeArray[5][3][0]
-        LegKneeAngRightMin = LegRangeArray[5][0][0]
-        LegKneeAngRightMax = LegRangeArray[5][1][0]
-
-        LegHipAngLeftMin = LegRangeArray[6][4][0]
-        LegHipAngLeftMax = LegRangeArray[6][5][0]
-        LegFootAngLeftMin = LegRangeArray[6][2][0]
-        LegFootAngLeftMax = LegRangeArray[6][3][0]
-        LegKneeAngLeftMin = LegRangeArray[6][0][0]
-        LegKneeAngLeftMax = LegRangeArray[6][1][0]
+        LegHipAngLeftMin = self._HomePositionArray[6][4][0]
+        LegHipAngLeftMax = self._HomePositionArray[6][5][0]
+        LegFootAngLeftMin = self._HomePositionArray[6][2][0]
+        LegFootAngLeftMax = self._HomePositionArray[6][3][0]
+        LegKneeAngLeftMin = self._HomePositionArray[6][0][0]
+        LegKneeAngLeftMax = self._HomePositionArray[6][1][0]
 
         if SwitchBothLeg == False:
 
